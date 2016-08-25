e02699ce776b544ff6fd467df30f2297a445475c
Tim Besard <tim.besard@gmail.com>
2016-08-25 13:05:16 -0400

    Rework and document build script.

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,14 @@
+# This script looks for LLVM installations and selects one based on the compatibility with
+# available wrappers.
+#
+# This is somewhat convoluted, as we can find libLLVM in a variety of places using different
+# mechanisms, while version matching needs to consider ABI compatibility.
+#
+# Environment variables influencing this process:
+#   DEBUG=1                     print debug information
+#   LLVM_VERSION=$MAJOR.$MINOR  only consider using a specific LLVM version
+#                               (which still needs to be compatible)
+
 using Compat
 import Compat.String
 
@@ -13,38 +24,44 @@ function libname(version::VersionNumber)
             "libLLVM-$(version.major).$(version.minor).$(version.patch).so"]
 end
 
-# This script looks for LLVM installations in a variety of places, and choses one (adhering
-# to LLVM_VERSION, if specified) in the following descending order of priority:
+
+#
+# Discovery
+#
+
+# Discover LLVM installations and their contained libraries, in the following order of
+# preference:
 # - shipped LLVM libraries (only versioned)
 # - system-wide LLVM libraries (only versioned)
 # - shipped llvm-config binaries (first versioned, then unversioned)
 # - system-wide llvm-config binaries (first versioned, then unversioned)
 #
 # Unversioned LLVM libraries are not considered, as the API doesn't seem to provide a way to
-# detect the LLVM version, and thus we can't select the appropriate wrapper.
-#
-# When selecting a final library, installations with a major and minor version matching
-# wrapped headers (see the `lib` folder) are considered first, in the order of preference
-# detailed above. If no such library is found, probably-compatible versions are considered
-# (unless the user specifically requested a version using the `LLVM_VERSION` environment
-# variable).
-
-# versions wrapped
-wrapped_versions = map(lib -> VersionNumber(lib),
-                       readdir(joinpath(dirname(@__FILE__), "..", "lib")))
-
-# versions to consider
-const requested_version = haskey(ENV, "LLVM_VERSION")
-if requested_version
-    debug("Overriding LLVM version requirement to v", ENV["LLVM_VERSION"])
-    ismatch(r"^\d.\d$", ENV["LLVM_VERSION"]) || error("invalid version requested (should be MAJOR.MINOR)")
-    versions = [VersionNumber(ENV["LLVM_VERSION"])]
-else
-    versions = wrapped_versions
-    debug("Acceptable LLVM versions: ", join(versions, ", "))
-end
+# detect the LLVM version (which we need to select an wrapper, or switch behavior).
 
 libraries = Vector{Tuple{String, VersionNumber}}()
+
+# LLVM versions to look for.
+#
+# Note that this list can be incomplete, as we'll still look for unversioned llvm-config
+# binaries which may yield different versions.
+acceptable_versions = [VersionNumber(Base.libllvm_version),
+                       v"4.0",
+                       v"3.9.0",
+                       v"3.8.1", v"3.8.0"]
+
+if haskey(ENV, "LLVM_VERSION")
+    ismatch(r"^\d.\d$", ENV["LLVM_VERSION"]) || error("invalid version requested (should be MAJOR.MINOR)")
+    requested_version = Nullable(VersionNumber(ENV["LLVM_VERSION"]))
+
+    # NOTE: even though we'll only care about the requested version, still look for others
+    #       as it may be useful for debugging purposes
+    push!(acceptable_versions, get(requested_version))
+else
+    requested_version = Nullable{VersionNumber}()
+end
+
+debug("Acceptable LLVM versions: ", join(acceptable_versions, ", "))
 
 # NOTE: we do manual library detection (ie. no find_librari, dlopen) for two reasons:
 # - ability to detect libraries of unknown versions (by scanning libdirs and regex matching)
@@ -71,7 +88,7 @@ for dir in unique(libdirs)
 end
 
 # guess for versioned libraries (as the user might have configured ld.so differently)
-for version in versions, name in libname(version)
+for version in acceptable_versions, name in libname(version)
     debug("Searching for library $name")
     lib = Libdl.dlopen_e(name)
     if lib != C_NULL
@@ -82,7 +99,6 @@ for version in versions, name in libname(version)
 end
 
 # check llvm-for config binaries in known locations
-configversions = [map(v->Nullable(v), versions)..., Nullable{VersionNumber}()]
 configdirs = [JULIA_HOME, joinpath(JULIA_HOME, "..", "tools"), split(ENV["PATH"], ':')...]
 for dir in unique(configdirs)
     isdir(dir) || continue
@@ -108,14 +124,14 @@ for dir in unique(configdirs)
         # deal with unversioned llvm-config binaries
         if isnull(version)
             config_version = VersionNumber(readchomp(`$config --version`))
-            debug("- reports LLVM v$config_version")
+            debug("... reports LLVM v$config_version")
         else
             config_version = get(version)
         end
 
         # check for libraries
         libdir = readchomp(`$config --libdir`)
-        debug("- contains libraries in $libdir")
+        debug("... contains libraries in $libdir")
         for name in [libname(config_version); libname()]
             lib = joinpath(libdir, name)
             if ispath(lib)
@@ -126,46 +142,69 @@ for dir in unique(configdirs)
     end
 end
 
-# select matching libraries
+info("Found $(length(libraries)) LLVM libraries, providing $(length(unique(map(t->t[2],libraries)))) different versions")
+
+
+#
+# Selection
+#
+
+# First consider installations with a major and minor version matching wrapped headers (see
+# the `lib` folder) are considered first. If no such libraries have been found, consider
+# probably-compatible versions (ie. for which we have an older set of wrapped headers).
+#
+# If the user requested a specific version, only ever consider that version.
+
 vercmp_match = (a,b) -> a.major==b.major && a.minor==b.minor
-matching_libraries = filter(t -> any(v -> vercmp_match(t[2],v), versions), libraries)
+vercmp_compat = (a,b) -> a.major>b.major || (a.major==b.major && a.minor>=b.minor)
+
+if !isnull(requested_version)
+    info("Overriding selection to match v$(get(requested_version))")
+    libraries = filter(t->vercmp_match(t[2],get(requested_version)), libraries)
+end
+
+# versions wrapped
+wrapped_versions = map(lib -> VersionNumber(lib),
+                       readdir(joinpath(dirname(@__FILE__), "..", "lib")))
+
+# select matching libraries
+matching_libraries = filter(t -> any(v -> vercmp_match(t[2],v), wrapped_versions), libraries)
 
 # select probably compatible libraries
-vercmp_compat = (a,b) -> a.major>b.major || (a.major==b.major && a.minor>=b.minor)
-compatible_libraries = filter(t -> any(v -> vercmp_compat(t[2],v), versions), libraries)
-
-# TODO: the compatibility selection is a bit messy, as we consider wrapped versions twice:
-#       first when selecting compatible libraries (versions=wrapped_versions),
-#       then when selecting wrappers
+compatible_libraries = filter(t -> any(v -> vercmp_compat(t[2],v), wrapped_versions), libraries)
 
 # select a compatible library
 if !isempty(matching_libraries)
-    (libllvm, version) = first(matching_libraries)
-elseif !isempty(compatible_libraries) && !requested_version
-    (libllvm, version) = first(compatible_libraries)
-    warn("could not find a matching LLVM installation, but did find a v$version installation which is probably compatible...")
+    (libllvm, libllvm_version) = first(matching_libraries)
+elseif !isempty(compatible_libraries)
+    (libllvm, libllvm_version) = first(compatible_libraries)
+    warn("could not find a matching LLVM installation, but did find a v$libllvm_version installation which is probably compatible...")
 else
     error("could not find a compatible LLVM installation")
 end
-info("Tuning for libLLVM v$version at $(realpath(libllvm))")
+info("Selected LLVM v$libllvm_version at $(realpath(libllvm))")
 
 # check if the library is wrapped
-wrapped_libdir = joinpath(dirname(@__FILE__), "..", "lib", verstr(version))
+wrapped_libdir = joinpath(dirname(@__FILE__), "..", "lib", verstr(libllvm_version))
 if isdir(wrapped_libdir)
-    wrapper_version = version
+    wrapper_version = libllvm_version
 else
     # the selected version is not supported, check for compatible (ie. older) wrapped
     # headers which are probably compatible (there's no real ABI guarantee though)...
-    debug("No wrapped headers for v$version found")
-    fallback_versions = filter(v->vercmp_compat(version, v), wrapped_versions)
-    if !isempty(fallback_versions)
-        fallback_version = last(fallback_versions)
-        wrapper_version = fallback_version
-        warn("LLVM v$version is not supported, falling back to support for v$fallback_version (file an issue if there's incompatibilities)")
-        wrapped_libdir = joinpath(dirname(@__FILE__), "..", "lib", verstr(fallback_version))
+    debug("No wrapped headers for v$libllvm_version found")
+    compatible_wrappers = filter(v->vercmp_compat(libllvm_version, v), wrapped_versions)
+    if !isempty(compatible_wrappers)
+        wrapper_version = last(compatible_wrappers)
+        warn("LLVM v$libllvm_version is not supported, falling back to support for v$wrapper_version (file an issue if there's incompatibilities)")
+        wrapped_libdir = joinpath(dirname(@__FILE__), "..", "lib", verstr(wrapper_version))
     end
 end
-isdir(wrapped_libdir) || error("LLVM v$version is not supported")
+isdir(wrapped_libdir) || error("LLVM v$libllvm_version is not supported")
+
+
+#
+# Finishing up
+#
 
 # write ext.jl
 wrapper_common = joinpath(wrapped_libdir, "libLLVM_common.jl")
@@ -173,8 +212,8 @@ wrapper_header = joinpath(wrapped_libdir, "libLLVM_h.jl")
 open(joinpath(dirname(@__FILE__), "ext.jl"), "w") do fh
     write(fh, """
         const libllvm = "$libllvm"
-        const libllvm_version = v"$version"
-        const libllvm_wrapper = v"$wrapper_version"
+        const libllvm_version = v"$libllvm_version"
+        const wrapper_version = v"$wrapper_version"
         include("$wrapper_common")
         include("$wrapper_header")""")
 end
