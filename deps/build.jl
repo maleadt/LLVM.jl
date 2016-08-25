06c7fa620824a56dfccc772ccb45385c20bd0803
Tim Besard <tim.besard@gmail.com>
2016-08-25 10:59:59 -0400

    Enable testing for LLVM 4.0, falling back to headers for 3.9.

--- deps/build.jl
+++ deps/build.jl
@@ -22,15 +22,25 @@ end
 #
 # Unversioned LLVM libraries are not considered, as the API doesn't seem to provide a way to
 # detect the LLVM version, and thus we can't select the appropriate wrapper.
+#
+# When selecting a final library, installations with a major and minor version matching
+# wrapped headers (see the `lib` folder) are considered first, in the order of preference
+# detailed above. If no such library is found, probably-compatible versions are considered
+# (unless the user specifically requested a version using the `LLVM_VERSION` environment
+# variable).
+
+# versions wrapped
+wrapped_versions = map(lib -> VersionNumber(lib),
+                       readdir(joinpath(dirname(@__FILE__), "..", "lib")))
 
 # versions to consider
-if haskey(ENV, "LLVM_VERSION")
+const requested_version = haskey(ENV, "LLVM_VERSION")
+if requested_version
     debug("Overriding LLVM version requirement to v", ENV["LLVM_VERSION"])
     ismatch(r"^\d.\d$", ENV["LLVM_VERSION"]) || error("invalid version requested (should be MAJOR.MINOR)")
     versions = [VersionNumber(ENV["LLVM_VERSION"])]
 else
-    versions = map(lib -> VersionNumber(lib),
-                   readdir(joinpath(dirname(@__FILE__), "..", "lib")))
+    versions = wrapped_versions
     debug("Acceptable LLVM versions: ", join(versions, ", "))
 end
 
@@ -116,16 +126,46 @@ for dir in unique(configdirs)
     end
 end
 
+# select matching libraries
+vercmp_match = (a,b) -> a.major==b.major && a.minor==b.minor
+matching_libraries = filter(t -> any(v -> vercmp_match(t[2],v), versions), libraries)
+
+# select probably compatible libraries
+vercmp_compat = (a,b) -> a.major>b.major || (a.major==b.major && a.minor>=b.minor)
+compatible_libraries = filter(t -> any(v -> vercmp_compat(t[2],v), versions), libraries)
+
+# TODO: the compatibility selection is a bit messy, as we consider wrapped versions twice:
+#       first when selecting compatible libraries (versions=wrapped_versions),
+#       then when selecting wrappers
+
 # select a compatible library
-vercmp = (a,b) -> a.major==b.major && a.minor==b.minor
-compat_libraries = filter(t -> any(v -> vercmp(t[2],v), versions), libraries)
-isempty(compat_libraries) && error("could not find a compatible LLVM installation")
-(libllvm, version) = first(compat_libraries)
+if !isempty(matching_libraries)
+    (libllvm, version) = first(matching_libraries)
+elseif !isempty(compatible_libraries) && !requested_version
+    (libllvm, version) = first(compatible_libraries)
+    warn("could not find a matching LLVM installation, but did find a v$version installation which is probably compatible...")
+else
+    error("could not find a compatible LLVM installation")
+end
 info("Tuning for libLLVM v$version at $(realpath(libllvm))")
 
 # check if the library is wrapped
 wrapped_libdir = joinpath(dirname(@__FILE__), "..", "lib", verstr(version))
-isdir(wrapped_libdir) || error("LLVM v$version is not supported, please file an issue")
+if isdir(wrapped_libdir)
+    wrapper_version = version
+else
+    # the selected version is not supported, check for compatible (ie. older) wrapped
+    # headers which are probably compatible (there's no real ABI guarantee though)...
+    debug("No wrapped headers for v$version found")
+    fallback_versions = filter(v->vercmp_compat(version, v), wrapped_versions)
+    if !isempty(fallback_versions)
+        fallback_version = last(fallback_versions)
+        wrapper_version = fallback_version
+        warn("LLVM v$version is not supported, falling back to support for v$fallback_version (file an issue if there's incompatibilities)")
+        wrapped_libdir = joinpath(dirname(@__FILE__), "..", "lib", verstr(fallback_version))
+    end
+end
+isdir(wrapped_libdir) || error("LLVM v$version is not supported")
 
 # write ext.jl
 wrapper_common = joinpath(wrapped_libdir, "libLLVM_common.jl")
@@ -134,6 +174,7 @@ open(joinpath(dirname(@__FILE__), "ext.jl"), "w") do fh
     write(fh, """
         const libllvm = "$libllvm"
         const libllvm_version = v"$version"
+        const libllvm_wrapper = v"$wrapper_version"
         include("$wrapper_common")
         include("$wrapper_header")""")
 end
