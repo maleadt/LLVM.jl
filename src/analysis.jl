1a39c632d7006804a0939317f15bd1cae85efb5f
Tim Besard <tim.besard@gmail.com>
2016-08-29 21:24:52 -0400

    Refactor Analysis.

--- src/analysis.jl
+++ src/analysis.jl
@@ -1,16 +1,15 @@
 export verify
 
 function verify(mod::Module)
-    message = Ref{Cstring}()
-    status = API.LLVMVerifyModule(ref(mod), API.LLVMReturnStatusAction, message)
+    outerror = Ref{Cstring}()
+    status =
+        BoolFromLLVM(API.LLVMVerifyModule(ref(mod), API.LLVMReturnStatusAction, outerror))
 
-    if status != 0
-        error = unsafe_string(message[])
-        # TODO: this message often contains endlines, wrap in LLVM error type
-        #       with a `showerror` rendering those endlines
-        API.LLVMDisposeMessage(message[])
+    if status
+        error = unsafe_string(outerror[])
+        API.LLVMDisposeMessage(outerror[])
         throw(error)
     else
-        API.LLVMDisposeMessage(message[])
+        API.LLVMDisposeMessage(outerror[])
     end
 end
