da6d2338efa10433a5a394c93aa9a38a0afc5536
Tim Besard <tim.besard@gmail.com>
2016-11-11 14:12:01 -0500

    Rework build script in anticipation of prebuilt artefacts.

--- deps/build.jl
+++ deps/build.jl
@@ -23,9 +23,10 @@ function libname(version::VersionNumber)
         return ["libLLVM.dylib"]
     elseif is_linux()
         # Linux DSO's aren't versioned, so only use versioned filenames
-        prerelease = join(version.prerelease)
-        return ["libLLVM-$(version.major).$(version.minor)$prerelease.so",
-                "libLLVM-$(version.major).$(version.minor).$(version.patch)$prerelease.so"]
+        return ["libLLVM-$(version.major).$(version.minor).$(version.patch).so",
+                "libLLVM-$(version.major).$(version.minor).$(version.patch)svn.so",
+                "libLLVM-$(version.major).$(version.minor).so",
+                "libLLVM-$(version.major).$(version.minor)svn.so"]
     else
         error("Unknown OS")
     end
@@ -33,22 +34,8 @@ end
 
 
 #
-# Discovery
+# Versions
 #
-
-# Discover LLVM installations and their contained libraries, by looking for llvm-config
-# binaries in the following order of precedence:
-# - shipped llvm-config binaries (first versioned, then unversioned)
-# - system-wide llvm-config binaries (first versioned, then unversioned)
-#
-# NOTE: we only look for llvm-config binaries as we need to compile sources with additional
-# API calls. If this isn't necessary anymore, we can also look for plain LLVM libraries.
-# This functionality was present in an early version of this file.
-#
-# NOTE: we also refuse to link against unversioned LLVM library files (even though
-# `llvm-config` told us the version), as that might conflict with files in the Julia dist.
-
-llvms = Vector{Tuple{String, String, VersionNumber}}()
 
 # LLVM versions to look for.
 #
@@ -61,7 +48,8 @@ acceptable_versions = [VersionNumber(Base.libllvm_version),
 sort!(acceptable_versions)
 
 if haskey(ENV, "LLVM_VERSION")
-    ismatch(r"^\d.\d$", ENV["LLVM_VERSION"]) || error("invalid version requested (should be MAJOR.MINOR)")
+    ismatch(r"^\d.\d$", ENV["LLVM_VERSION"]) ||
+        error("invalid version requested (should be MAJOR.MINOR)")
     requested_version = Nullable(VersionNumber(ENV["LLVM_VERSION"]))
 
     # NOTE: even though we'll only consider the requested version,
@@ -73,49 +61,81 @@ end
 
 debug("Acceptable LLVM versions: ", join(acceptable_versions, ", "))
 
-# check for llvm-config binaries in known locations
-configdirs = [JULIA_HOME, joinpath(JULIA_HOME, "..", "tools"), split(ENV["PATH"], ':')...]
-for dir in unique(configdirs)
-    isdir(dir) || continue
-    debug("Searching for config binaries in $dir")
 
-    # first discover llvm-config binaries
+
+#
+# Discovery
+#
+
+immutable Toolchain
+    config::Nullable{String}
+    library::String
+    version::VersionNumber
+end
+llvms = Vector{Toolchain}()
+
+# returns vector of Tuple{path::String, VersionNumber}
+function find_libllvm(dir::String, versions::Vector{VersionNumber})
+    debug("Looking for libLLVM in $dir")
+    libraries = Vector{Tuple{String, VersionNumber}}()
+    for version in versions, name in libname(version)
+        lib = joinpath(dir, name)
+        if ispath(lib)
+            debug("- v$version at $lib")
+            push!(libraries, tuple(lib, version))
+        end
+    end
+
+    return libraries
+end
+
+# returns vector of Tuple{path::String, VersionNumber}
+function find_llvmconfig(dir::String)
+    debug("Looking for llvm-config in $dir")
     configs = Vector{Tuple{String, VersionNumber}}()
     for file in readdir(dir)
         if startswith(file, "llvm-config")
             path = joinpath(dir, file)
             version = VersionNumber(strip(readstring(`$path --version`)))
-            debug("- found llvm-config at $path")
+            debug("- $version at $path")
             push!(configs, tuple(path, version))
         end
     end
 
-    # then discover libraries
-    for (config, config_version) in configs
-        debug("Searching for libraries using $config reporting $config_version")
+    return configs
+end
 
-        # prerelease versions have an "svn" tag
-        library_versions = [config_version,
-                            VersionNumber(config_version.major, config_version.minor,
-                                          config_version.patch, ("svn",))]
+# check for versioned LLVM libraries in known locations
+libdirs = [joinpath(JULIA_HOME, "..", "lib", "julia")]
+for libdir in libdirs
+    libraries = find_libllvm(libdir, acceptable_versions)
 
-        # check for libraries
+    for (library, version) in libraries
+        push!(llvms, Toolchain(Nullable{String}(), library, version))
+    end
+end
+
+# check for llvm-config binaries in known locations
+configdirs = [JULIA_HOME, joinpath(JULIA_HOME, "..", "tools"), split(ENV["PATH"], ':')...]
+for dir in unique(configdirs)
+    isdir(dir) || continue
+    configs = find_llvmconfig(dir)
+
+    # look for libraries in the reported library directory
+    for (config, version) in configs
         libdir = readchomp(`$config --libdir`)
-        debug("... contains libraries in $libdir")
-        for library_version in library_versions, name in libname(library_version)
-            # NOTE: library_version is only used to find the library file names,
-            #       the actual version is assumed to be what `llvm-config` reported
-            lib = joinpath(libdir, name)
-            entry = tuple(lib, config, config_version)
-            if ispath(lib) && !(entry in llvms)
-                debug("- found v$config_version at $lib")
-                push!(llvms, entry)
-            end
+        libraries = find_libllvm(libdir, [version])
+
+        for (library, _) in libraries
+            push!(llvms, Toolchain(config, library, version))
         end
     end
 end
 
-info("Found $(length(llvms)) LLVM installations, providing $(length(unique(map(t->t[3],llvms)))) different versions")
+# prune
+llvms = unique(x -> realpath(x.library), llvms)
+
+info("Found $(length(llvms)) unique LLVM installations")
 
 
 #
@@ -128,12 +148,12 @@ info("Found $(length(llvms)) LLVM installations, providing $(length(unique(map(t
 #
 # If the user requested a specific version, only ever consider that version.
 
-vercmp_match = (a,b) -> a.major==b.major && a.minor==b.minor
-vercmp_compat = (a,b) -> a.major>b.major || (a.major==b.major && a.minor>=b.minor)
+vercmp_match  = (a,b) -> a.major==b.major &&  a.minor==b.minor
+vercmp_compat = (a,b) -> a.major>b.major  || (a.major==b.major && a.minor>=b.minor)
 
 if !isnull(requested_version)
     info("Overriding selection to match v$(get(requested_version))")
-    llvms = filter(t->vercmp_match(t[3],get(requested_version)), llvms)
+    llvms = filter(t->vercmp_match(t.version,get(requested_version)), llvms)
 end
 
 # versions wrapped
@@ -142,37 +162,33 @@ wrapped_versions = map(dir->VersionNumber(dir),
                               readdir(joinpath(@__DIR__, "..", "lib"))))
 
 # select wrapper
-matching_llvms = filter(t -> any(v -> vercmp_match(t[3],v), wrapped_versions), llvms)
-compatible_llvms = filter(t -> any(v -> vercmp_compat(t[3],v), wrapped_versions), llvms)
-if !isempty(matching_llvms)
-    (llvm_library, llvm_config, llvm_version) = first(matching_llvms)
-    wrapper_version = llvm_version
-elseif !isempty(compatible_llvms)
-    (llvm_library, llvm_config, llvm_version) = first(compatible_llvms)
-    compatible_wrappers = filter(v->vercmp_compat(llvm_version, v), wrapped_versions)
-    wrapper_version = last(compatible_wrappers)
-    warn("LLVM v$llvm_version is not supported, falling back to support for v$wrapper_version (file an issue if there's incompatibilities)")
+matching_llvms   = filter(t -> any(v -> vercmp_match(t.version,v), wrapped_versions), llvms)
+compatible_llvms = filter(t -> !in(t, matching_llvms) && 
+                               any(v -> vercmp_compat(t.version,v), wrapped_versions), llvms)
+
+if isempty(matching_llvms)
+    if isempty(compatible_llvms)
+        error("could not find a compatible LLVM installation")
+    else
+        warn("could not find a matching LLVM installation, falling back on probably-compatible ones")
+        llvms = compatible_llvms
+    end
 else
-    error("could not find a compatible LLVM installation")
+    llvms = [matching_llvms; compatible_llvms]
 end
 
-wrapped_libdir = joinpath(@__DIR__, "..", "lib", verstr(wrapper_version))
-@assert isdir(wrapped_libdir)
-
-# sanity check: open the library
-# NOTE: can't do this because LLVM 3.9 might link against libLTO (see Makefile workaround)
-# debug("Opening library")
-# Libdl.dlopen(llvm_library)
-
 
 #
-# Finishing up
+# Build
 #
 
-llvm_targets = Symbol.(split(readstring(`$llvm_config --targets-built`)))
+# at this point, we require `llvm-config` for building
+filter!(x->!isnull(x.config), llvms)
+isempty(llvms) && error("could not find LLVM installation providing llvm-config (required to build libLLVM_extra)")
 
-# don't include the patch number in the compatibility version string
-llvm_verstr = "$(llvm_version.major).$(llvm_version.minor)"
+# pick the first version and run with it (we should be able to build with all of them)
+llvm = first(llvms)
+info("Building for LLVM v$(llvm.version) at $(llvm.library) using $(get(llvm.config))")
 
 # location of julia-config.jl
 julia_cmd = Base.julia_cmd()
@@ -182,10 +198,10 @@ julia_config = joinpath(JULIA_HOME, "..", "share", "julia", "julia-config.jl")
 isfile(julia_config) || error("could not find julia-config.jl relative to $(JULIA_HOME) (note that in-source builds are only supported on Julia 0.6+)")
 
 # build library with extra functions
-libllvm_extra = joinpath(@__DIR__, "llvm-extra", "libLLVM_extra-$(llvm_verstr).$libext")
+libllvm_extra = joinpath(@__DIR__, "llvm-extra", "libLLVM_extra-$(verstr(llvm.version)).$libext")
 cd(joinpath(@__DIR__, "llvm-extra")) do
-    withenv("LLVM_CONFIG" => llvm_config,
-            "LLVM_LIBRARY" => llvm_library, "LLVM_VERSION" => llvm_verstr,
+    withenv("LLVM_CONFIG" => get(llvm.config),
+            "LLVM_LIBRARY" => llvm.library, "LLVM_VERSION" => verstr(llvm.version),
             "JULIA_CONFIG" => julia_config, "JULIA" => julia) do
         # force a rebuild as the LLVM installation might have changed, undetectably
         run(`make clean`)
@@ -197,8 +213,27 @@ end
 debug("Opening wrapper library")
 Libdl.dlopen(libllvm_extra)
 
-llvm_library_mtime = stat(llvm_library).mtime
 
+#
+# Finishing up
+#
+
+# gather libLLVM information
+llvm_targets = Symbol.(split(readstring(`$(get(llvm.config)) --targets-built`)))
+llvm_library_mtime = stat(llvm.library).mtime
+
+# select a wrapper
+if llvm.version in wrapped_versions
+    wrapper_version = llvm.version
+else
+    compatible_wrappers = filter(v->vercmp_compat(llvm.version, v), wrapped_versions)
+    wrapper_version = last(compatible_wrappers)
+    debug("Will be using wrapper v$(verstr(wrapper_version)) for libLLVM v$(llvm.version)")
+end
+wrapped_libdir = joinpath(@__DIR__, "..", "lib", verstr(wrapper_version))
+@assert isdir(wrapped_libdir)
+
+# gather wrapper information
 libllvm_wrapper_common = joinpath(wrapped_libdir, "libLLVM_common.jl")
 libllvm_wrapper = joinpath(wrapped_libdir, "libLLVM_h.jl")
 libllvm_extra_wrapper = joinpath(wrapped_libdir, "..", "libLLVM_extra.jl")
@@ -207,8 +242,8 @@ libllvm_extra_wrapper = joinpath(wrapped_libdir, "..", "libLLVM_extra.jl")
 open(joinpath(@__DIR__, "ext.jl"), "w") do fh
     write(fh, """
         # LLVM library properties
-        const libllvm_version = v"$llvm_version"
-        const libllvm_path = "$llvm_library"
+        const libllvm_version = v"$(llvm.version)"
+        const libllvm_path = "$(llvm.library)"
         const libllvm_mtime = $llvm_library_mtime
 
         # wrapper properties
