cb97fc9d55d531491ba57175d0eaec4860ad1a91
Tim Besard <tim.besard@gmail.com>
2017-06-08 15:04:32 +0200

    Pass Julia cmdline opts when running examples.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,18 @@ using Base.Test
 
 using Compat
 
+function julia_cmd(cmd)
+    return `
+        $(Base.julia_cmd())
+        --color=$(Base.have_color ? "yes" : "no")
+        --compilecache=$(Bool(Base.JLOptions().use_compilecache) ? "yes" : "no")
+        --history-file=no
+        --startup-file=$(Base.JLOptions().startupfile != 2 ? "yes" : "no")
+        --code-coverage=$(["none", "user", "all"][1+Base.JLOptions().code_coverage])
+        $cmd
+    `
+end
+
 @testset "LLVM" begin
 
 @testset "types" begin
