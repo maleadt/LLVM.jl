72b8be749a4e935bfceb74e5f6475ab1881e3a82
Tim Besard <tim.besard@gmail.com>
2017-09-18 17:04:51 +0200

    Remove ccall logging.
    It is fragile, and we'll need to move away from it anyway.

--- src/base.jl
+++ src/base.jl
@@ -7,21 +7,19 @@
 
 const libllvm = Ref{Ptr{Void}}()
 
-macro apicall(funspec, rettyp, argtypes, args...)
-    fun = if VERSION >= v"0.7.0-DEV.1729"
-        isa(funspec, QuoteNode) || error("first argument to @apicall should be a symbol")
-        funspec.value
+macro apicall(fun, rettyp, argtypes, args...)
+    if VERSION >= v"0.7.0-DEV.1729"
+        isa(fun, QuoteNode) || error("first argument to @apicall should be a symbol")
     else
-        if !isa(funspec, Expr) || funspec.head != :quote
+        if !isa(fun, Expr) || fun.head != :quote
             error("first argument to @apicall should be a symbol")
         end
-        funspec.args[1]
     end
 
     configured || return :(error("LLVM.jl has not been configured."))
 
     return quote
-        @logging_ccall($fun, Libdl.dlsym(libllvm[], $fun),
-                       $(esc(rettyp)), $(esc(argtypes)), $(map(esc, args)...))
+        ccall(Libdl.dlsym(libllvm[], $fun), $(esc(rettyp)),
+              $(esc(argtypes)), $(map(esc, args)...))
     end
 end
