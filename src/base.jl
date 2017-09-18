a580f19e619bdb121abf43e60593c04c4a3ba699
Tim Besard <tim.besard@gmail.com>
2017-09-18 16:28:15 +0200

    Fixes for julia master.

--- src/base.jl
+++ src/base.jl
@@ -7,9 +7,15 @@
 
 const libllvm = Ref{Ptr{Void}}()
 
-macro apicall(fun, rettyp, argtypes, args...)
-    if !isa(fun, Expr) || fun.head != :quote
-        error("first argument to @apicall should be a symbol")
+macro apicall(funspec, rettyp, argtypes, args...)
+    fun = if VERSION >= v"0.7.0-DEV.1729"
+        isa(funspec, QuoteNode) || error("first argument to @apicall should be a symbol")
+        funspec.value
+    else
+        if !isa(funspec, Expr) || funspec.head != :quote
+            error("first argument to @apicall should be a symbol")
+        end
+        funspec.args[1]
     end
 
     configured || return :(error("LLVM.jl has not been configured."))
