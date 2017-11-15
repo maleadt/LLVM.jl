e171970f8afd0a216396745c5239c120f0fa6ac1
autodocs <autodocs>
2017-11-15 14:53:36 +0000

    build based on b6b8b38

--- latest/search_index.js
+++ latest/search_index.js
@@ -88,4 +88,76 @@ var documenterSearchIndex = {"docs": [
     "text": "This section lists the package's public functionality that directly corresponds to functionality of the LLVM C API. In general, the abstractions stay close to those of the LLVM APIs, so for more information on certain library calls you can consult the LLVM C API reference.The documentation is grouped according to the modules of the driver API.WIP"
 },
 
+{
+    "location": "lib/interop.html#",
+    "page": "Julia/LLVM interop",
+    "title": "Julia/LLVM interop",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "lib/interop.html#Julia/LLVM-interop-1",
+    "page": "Julia/LLVM interop",
+    "title": "Julia/LLVM interop",
+    "category": "section",
+    "text": "This section lists functionality for connecting Julia with LLVM.jl, e.g. emitting code for the Julia JIT or creating types that are compatible with Julia's global state."
+},
+
+{
+    "location": "lib/interop.html#LLVM.Interop.JuliaContext",
+    "page": "Julia/LLVM interop",
+    "title": "LLVM.Interop.JuliaContext",
+    "category": "Function",
+    "text": "JuliaContext()\n\nReturns the (session-bound) LLVM context used by the Julia compiler.\n\n\n\n"
+},
+
+{
+    "location": "lib/interop.html#Base.convert-Tuple{Type{LLVM.LLVMType},Type}",
+    "page": "Julia/LLVM interop",
+    "title": "Base.convert",
+    "category": "Method",
+    "text": "convert(LLVMType, typ::Type)\n\nConvert a Julia type typ to its LLVM representation. Fails if the type would be boxed.\n\n\n\n"
+},
+
+{
+    "location": "lib/interop.html#LLVM.Interop.create_function",
+    "page": "Julia/LLVM interop",
+    "title": "LLVM.Interop.create_function",
+    "category": "Function",
+    "text": "create_function(rettyp::LLVMType, argtyp::Vector{LLVMType}, [name::String])\n\nCreate an LLVM function, given its return type rettyp and a vector of argument types argtyp. The function is marked for inlining, to be embedded in the caller's body. Returns both the newly created function, and its type.\n\n\n\n"
+},
+
+{
+    "location": "lib/interop.html#LLVM.Interop.call_function",
+    "page": "Julia/LLVM interop",
+    "title": "LLVM.Interop.call_function",
+    "category": "Function",
+    "text": "call_function(f::LLVM.Function, rettyp::Type, argtyp::Type, args::Expr)\n\nGenerate a call to an LLVM function f, given its return type rettyp and a tuple-type for the arguments. The arguments should be passed as an expression yielding a tuple of the argument values (eg. :((1,2))), which will be splatted into the call to the function.\n\n\n\n"
+},
+
+{
+    "location": "lib/interop.html#Base-functionality-1",
+    "page": "Julia/LLVM interop",
+    "title": "Base functionality",
+    "category": "section",
+    "text": "LLVM.Interop.JuliaContext\nBase.convert(::Type{LLVMType}, ::Type)\nLLVM.Interop.create_function\nLLVM.Interop.call_function"
+},
+
+{
+    "location": "lib/interop.html#LLVM.Interop.@asmcall",
+    "page": "Julia/LLVM interop",
+    "title": "LLVM.Interop.@asmcall",
+    "category": "Macro",
+    "text": "@asmcall asm::String [constraints::String] [side_effects::Bool=false]\n         rettyp=Void argtyp=Tuple{} args...\n\nCall some inline assembly asm, optionally constrained by constraints and denoting other side_effects in side_effects, specifying the return type in rettyp and types of arguments as a tuple-type in argtyp.\n\n\n\n"
+},
+
+{
+    "location": "lib/interop.html#Inline-assembly-1",
+    "page": "Julia/LLVM interop",
+    "title": "Inline assembly",
+    "category": "section",
+    "text": "LLVM.Interop.@asmcall"
+},
+
 ]}
