21e5a45ef87503856f9238c28f6359e705a2a6cf
autodocs <autodocs>
2018-03-26 20:27:15 +0000

    build based on 705f5c7

--- stable/search_index.js
+++ stable/search_index.js
@@ -61,7 +61,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Troubleshooting",
     "title": "Building llvm-extra fails due to C++11 ABI issues",
     "category": "section",
-    "text": "The build step might fail at building the llvm-extra wrapper with errors like the following:IR/Pass.o:(.data.rel.ro._ZTVN4llvm15JuliaModulePassE[_ZTVN4llvm15JuliaModulePassE]+0x40): undefined reference to `llvm::ModulePass::createPrinterPass(llvm::raw_ostream&, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > const&) const'\nIR/Pass.o:(.data.rel.ro._ZTVN4llvm17JuliaFunctionPassE[_ZTVN4llvm17JuliaFunctionPassE]+0x40): undefined reference to `llvm::FunctionPass::createPrinterPass(llvm::raw_ostream&, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > const&) const'\nIR/Pass.o:(.data.rel.ro._ZTVN4llvm19JuliaBasicBlockPassE[_ZTVN4llvm19JuliaBasicBlockPassE]+0x40): undefined reference to `llvm::BasicBlockPass::createPrinterPass(llvm::raw_ostream&, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > const&) const'\ncollect2: error: ld returned 1 exit statusIR/Pass.o:(.data.rel.ro._ZTVN4llvm15JuliaModulePassE[_ZTVN4llvm15JuliaModulePassE]+0x40): undefined reference to `llvm::ModulePass::createPrinterPass(llvm::raw_ostream&, std::string const&) const'\nIR/Pass.o:(.data.rel.ro._ZTVN4llvm17JuliaFunctionPassE[_ZTVN4llvm17JuliaFunctionPassE]+0x40): undefined reference to `llvm::FunctionPass::createPrinterPass(llvm::raw_ostream&, std::string const&) const'\nIR/Pass.o:(.data.rel.ro._ZTVN4llvm19JuliaBasicBlockPassE[_ZTVN4llvm19JuliaBasicBlockPassE]+0x40): undefined reference to `llvm::BasicBlockPass::createPrinterPass(llvm::raw_ostream&, std::string const&) const'\ncollect2: error: ld returned 1 exit statusThese indicate a mismatch between the C++ ABI of the LLVM library (more specifically, caused by the C++11 ABI change), and what your compiler selects by default. The Makefile in this package tries to detect any C++11 ABI symbols in the selected LLVM library and configures GLIBC accordingly, but this detection might fail when objdump is not available on your system, or might not help if the target compiler doesn't support said ABI.Most if these issues can be fixed by using the same compiler LLVM was build with to compile llvm-extra. You can override the selected compiler by defining the CC and CXX environment variables, eg. CC=clang CXX=clang++ julia -e 'Pkg.build(\"LLVM\")'."
+    "text": "The build step might fail at building the llvm-extra wrapper with errors like the following:IR/Pass.o:(.data.rel.ro._ZTVN4llvm15JuliaModulePassE[_ZTVN4llvm15JuliaModulePassE]+0x40): undefined reference to `llvm::ModulePass::createPrinterPass(llvm::raw_ostream&, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > const&) const\'\nIR/Pass.o:(.data.rel.ro._ZTVN4llvm17JuliaFunctionPassE[_ZTVN4llvm17JuliaFunctionPassE]+0x40): undefined reference to `llvm::FunctionPass::createPrinterPass(llvm::raw_ostream&, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > const&) const\'\nIR/Pass.o:(.data.rel.ro._ZTVN4llvm19JuliaBasicBlockPassE[_ZTVN4llvm19JuliaBasicBlockPassE]+0x40): undefined reference to `llvm::BasicBlockPass::createPrinterPass(llvm::raw_ostream&, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > const&) const\'\ncollect2: error: ld returned 1 exit statusIR/Pass.o:(.data.rel.ro._ZTVN4llvm15JuliaModulePassE[_ZTVN4llvm15JuliaModulePassE]+0x40): undefined reference to `llvm::ModulePass::createPrinterPass(llvm::raw_ostream&, std::string const&) const\'\nIR/Pass.o:(.data.rel.ro._ZTVN4llvm17JuliaFunctionPassE[_ZTVN4llvm17JuliaFunctionPassE]+0x40): undefined reference to `llvm::FunctionPass::createPrinterPass(llvm::raw_ostream&, std::string const&) const\'\nIR/Pass.o:(.data.rel.ro._ZTVN4llvm19JuliaBasicBlockPassE[_ZTVN4llvm19JuliaBasicBlockPassE]+0x40): undefined reference to `llvm::BasicBlockPass::createPrinterPass(llvm::raw_ostream&, std::string const&) const\'\ncollect2: error: ld returned 1 exit statusThese indicate a mismatch between the C++ ABI of the LLVM library (more specifically, caused by the C++11 ABI change), and what your compiler selects by default. The Makefile in this package tries to detect any C++11 ABI symbols in the selected LLVM library and configures GLIBC accordingly, but this detection might fail when objdump is not available on your system, or might not help if the target compiler doesn\'t support said ABI.Most if these issues can be fixed by using the same compiler LLVM was build with to compile llvm-extra. You can override the selected compiler by defining the CC and CXX environment variables, eg. CC=clang CXX=clang++ julia -e \'Pkg.build(\"LLVM\")\'."
 },
 
 {
@@ -77,7 +77,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API wrappers",
     "title": "API wrappers",
     "category": "section",
-    "text": "This section lists the package's public functionality that directly corresponds to functionality of the LLVM C API. In general, the abstractions stay close to those of the LLVM APIs, so for more information on certain library calls you can consult the LLVM C API reference.The documentation is grouped according to the modules of the driver API.WIP"
+    "text": "This section lists the package\'s public functionality that directly corresponds to functionality of the LLVM C API. In general, the abstractions stay close to those of the LLVM APIs, so for more information on certain library calls you can consult the LLVM C API reference.The documentation is grouped according to the modules of the driver API.WIP"
 },
 
 {
@@ -93,39 +93,39 @@ var documenterSearchIndex = {"docs": [
     "page": "Julia/LLVM interop",
     "title": "Julia/LLVM interop",
     "category": "section",
-    "text": "This section lists functionality for connecting Julia with LLVM.jl, e.g. emitting code for the Julia JIT or creating types that are compatible with Julia's global state."
+    "text": "This section lists functionality for connecting Julia with LLVM.jl, e.g. emitting code for the Julia JIT or creating types that are compatible with Julia\'s global state."
 },
 
 {
     "location": "lib/interop.html#LLVM.Interop.JuliaContext",
     "page": "Julia/LLVM interop",
     "title": "LLVM.Interop.JuliaContext",
-    "category": "Function",
-    "text": "JuliaContext()\n\nReturns the (session-bound) LLVM context used by the Julia compiler.\n\n\n\n"
+    "category": "function",
+    "text": "JuliaContext()\n\nReturns the (session-bound) LLVM context used by the Julia compiler.\n\n\n\n\n\n"
 },
 
 {
     "location": "lib/interop.html#Base.convert-Tuple{Type{LLVMType},Type}",
     "page": "Julia/LLVM interop",
     "title": "Base.convert",
-    "category": "Method",
-    "text": "convert(LLVMType, typ::Type)\n\nConvert a Julia type typ to its LLVM representation. Fails if the type would be boxed.\n\n\n\n"
+    "category": "method",
+    "text": "convert(LLVMType, typ::Type)\n\nConvert a Julia type typ to its LLVM representation. Fails if the type would be boxed.\n\n\n\n\n\n"
 },
 
 {
     "location": "lib/interop.html#LLVM.Interop.create_function",
     "page": "Julia/LLVM interop",
     "title": "LLVM.Interop.create_function",
-    "category": "Function",
-    "text": "create_function(rettyp::LLVMType, argtyp::Vector{LLVMType}, [name::String])\n\nCreate an LLVM function, given its return type rettyp and a vector of argument types argtyp. The function is marked for inlining, to be embedded in the caller's body. Returns both the newly created function, and its type.\n\n\n\n"
+    "category": "function",
+    "text": "create_function(rettyp::LLVMType, argtyp::Vector{LLVMType}, [name::String])\n\nCreate an LLVM function, given its return type rettyp and a vector of argument types argtyp. The function is marked for inlining, to be embedded in the caller\'s body. Returns both the newly created function, and its type.\n\n\n\n\n\n"
 },
 
 {
     "location": "lib/interop.html#LLVM.Interop.call_function",
     "page": "Julia/LLVM interop",
     "title": "LLVM.Interop.call_function",
-    "category": "Function",
-    "text": "call_function(f::LLVM.Function, rettyp::Type, argtyp::Type, args::Expr)\n\nGenerate a call to an LLVM function f, given its return type rettyp and a tuple-type for the arguments. The arguments should be passed as an expression yielding a tuple of the argument values (eg. :((1,2))), which will be splatted into the call to the function.\n\n\n\n"
+    "category": "function",
+    "text": "call_function(f::LLVM.Function, rettyp::Type, argtyp::Type, args::Expr)\n\nGenerate a call to an LLVM function f, given its return type rettyp and a tuple-type for the arguments. The arguments should be passed as an expression yielding a tuple of the argument values (eg. :((1,2))), which will be splatted into the call to the function.\n\n\n\n\n\n"
 },
 
 {
@@ -140,8 +140,8 @@ var documenterSearchIndex = {"docs": [
     "location": "lib/interop.html#LLVM.Interop.@asmcall",
     "page": "Julia/LLVM interop",
     "title": "LLVM.Interop.@asmcall",
-    "category": "Macro",
-    "text": "@asmcall asm::String [constraints::String] [side_effects::Bool=false]\n         rettyp=Nothing argtyp=Tuple{} args...\n\nCall some inline assembly asm, optionally constrained by constraints and denoting other side_effects in side_effects, specifying the return type in rettyp and types of arguments as a tuple-type in argtyp.\n\n\n\n"
+    "category": "macro",
+    "text": "@asmcall asm::String [constraints::String] [side_effects::Bool=false]\n         rettyp=Nothing argtyp=Tuple{} args...\n\nCall some inline assembly asm, optionally constrained by constraints and denoting other sideeffects in `sideeffects, specifying the return type inrettypand types of arguments as a tuple-type inargtyp`.\n\n\n\n\n\n"
 },
 
 {
