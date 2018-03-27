e61de9819003e1914792e7d36a851ac9d7279ec7
Tim Besard <tim.besard@gmail.com>
2018-03-27 16:20:37 -0700

    Remove deprecated functions.

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,12 +1,1 @@
 # deprecated methods
-
-export populate!
-
-@deprecate populate!(pm::PassManager, tm::TargetMachine) add_transform_info!(pm, tm)
-
-import Base: get
-
-@deprecate get(iter::ModuleTypeDict, name::String) getindex(iter, name)
-@deprecate get(iter::ModuleMetadataDict, name::String) getindex(iter, name)
-@deprecate get(iter::ModuleGlobalSet, name::String) getindex(iter, name)
-@deprecate get(iter::ModuleFunctionSet, name::String) getindex(iter, name)
