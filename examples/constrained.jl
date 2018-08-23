de8531b69588c200e55bb26c0c5c900b50b98de6
Tim Besard <tim.besard@gmail.com>
2018-08-23 09:59:12 +0200

    Some clean-ups.

--- examples/constrained.jl
+++ examples/constrained.jl
@@ -1,24 +1,25 @@
 # Example on how access experimental intrinsic in LLVM
-# Note:
-#    that is example is using the constrained floating point instrinisc
-#    https://releases.llvm.org/6.0.0/docs/LangRef.html#constrained-floating-point-intrinsics
-#    but this will only change compile time behaviour/optimisations and not the actual runtime behaviour.
-#    For changing runtime behaviour please use https://github.com/JuliaIntervals/SetRounding.jl/blob/master/src/SetRounding.jl
-#    or a similar mechanism.
-# Note:
-#     This example is not complete and could use some better error handling
+#
+# This example uses the constrained floating point intrinsics
+# https://releases.llvm.org/6.0.0/docs/LangRef.html#constrained-floating-point-intrinsics
+#
+# Note that this will only change compile time behavior/optimizations and not the actual
+# runtime behavior. For changing runtime behavior please use the SetRounding.jl package or a
+# similar mechanism.
+#
+# This example is not complete and could use some better error handling
 
 using LLVM
 using LLVM.Interop
 
-# Select the right intrinsic overload https://github.com/maleadt/LLVM.jl/issues/112
+# TODO: select the right intrinsic overload (https://github.com/maleadt/LLVM.jl/issues/112)
 suffix(::Type{Float64}) = "f64"
 suffix(::Type{Float32}) = "f32"
 
-# Map Julia functions to llvm intrinsic
+# map Julia functions to llvm intrinsic
 func(::Type{typeof(+)}) = "fadd"
 
-# Stub of a Trait system for the different modes
+# stub of a Trait system for the different modes
 abstract type Round end
 abstract type FPExcept end
 
@@ -28,16 +29,17 @@ struct FPExceptStrict <: FPExcept end
 meta(::Type{RoundUpward}) = "round.upward"
 meta(::Type{FPExceptStrict}) = "fpexcept.strict"
 
-@generated function constrained(::F, ::Type{round}, ::Type{fpexcept}, xs::Vararg{T,N}) where {F, round, fpexcept, T<:AbstractFloat, N}
+@generated function constrained(::F, ::Type{round}, ::Type{fpexcept}, xs::Vararg{T,N}) where
+                               {F, round, fpexcept, T<:AbstractFloat, N}
     @assert N >= 0
-    ctx = JuliaContext()
 
-    # Create a llvm function for which we will build IR
     typ = convert(LLVMType, T)
+
+    # create a function
     paramtyps = [typ for i in 1:N]
     llvmf, _ = create_function(typ, paramtyps)
 
-    # Create the declaration of the instrinisc to be called
+    # create the intrinsic
     mtyp = LLVM.MetadataType(JuliaContext())
     mround = MDString(meta(round), JuliaContext())
     mfpexcept = MDString(meta(fpexcept), JuliaContext())
@@ -59,9 +61,9 @@ end
 cadd(x, y) = constrained(+, RoundUpward, FPExceptStrict, x, y)
 
 using Test
-using InteractiveUtils
 @test cadd(1.0, 2.0) == 3.0
 
+using InteractiveUtils
 io = IOBuffer()
 code_llvm(io, cadd, (Float32, Float32))
 seekstart(io)
