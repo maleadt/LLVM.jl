e88958a31b59ec6c9e7c0d495b2a644bd342ac77
Tim Besard <tim.besard@gmail.com>
2019-04-01 16:03:50 +0200

    Add high-level wrappers for the Julia passes.

--- src/interop.jl
+++ src/interop.jl
@@ -1,11 +1,13 @@
 module Interop
 
 using ..LLVM
+import ..LLVM: API, ref
 
 
 const jlctx = Ref{LLVM.Context}()
 
 include("interop/base.jl")
 include("interop/asmcall.jl")
+include("interop/passes.jl")
 
 end
