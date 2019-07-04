84f1257af71202be1db8b6d6c145dea706f3d68e
Valentin Churavy <v.churavy@gmail.com>
2019-07-04 02:01:46 -0400

    set module flags

--- src/core/module.jl
+++ src/core/module.jl
@@ -176,3 +176,9 @@ function Base.getindex(iter::ModuleFunctionSet, name::String)
     objref == C_NULL && throw(KeyError(name))
     return Function(objref)
 end
+
+# flags
+if libllvm_version >= v"8.0.0"
+addflag!(mod::Module, behavior, key, val) = LLVM.API.LLVMAddModuleFlag(ref(mod), behavior, key, length(key), LLVM.ref(Metadata(val)))
+
+end
