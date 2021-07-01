09859129819a38b96ede84f04c74bb2d83c4bbe8
Tim Besard <tim.besard@gmail.com>
2021-07-01 15:29:16 +0200

    Support for Julia with LLVM assertions.

--- deps/build_local.jl
+++ deps/build_local.jl
@@ -1,27 +1,45 @@
 # Invoke with
 # `julia --project=deps deps/build_local.jl`
 
-using Pkg, Scratch, Preferences, LLVMExtra_jll
+# the pre-built LLVMExtra_jll might not be loadable on this platform
+LLVMExtra_jll = Base.UUID("dad2f222-ce93-54a1-a47d-0025e8a3acab")
 
-# 1. Ensure that LLVM_full_jll is installed
-Pkg.add(name="LLVM_full_jll", version=Base.libllvm_version)
-using LLVM_full_jll
-LLVM_DIR = joinpath(LLVM_full_jll.artifact_dir, "lib", "cmake", "llvm")
+using Pkg, Scratch, Preferences, Libdl
+
+# 1. Ensure that an appropriate LLVM_full_jll is installed
+llvm_assertions = try
+    cglobal(:_ZN4llvm24DisableABIBreakingChecksE, Cvoid)
+    false
+catch
+    true
+end
+LLVM = if llvm_assertions
+    Pkg.add(name="LLVM_full_assert_jll", version=Base.libllvm_version)
+    using LLVM_full_assert_jll
+    LLVM_full_assert_jll
+else
+    Pkg.add(name="LLVM_full_jll", version=Base.libllvm_version)
+    using LLVM_full_jll
+    LLVM_full_jll
+end
+LLVM_DIR = joinpath(LLVM.artifact_dir, "lib", "cmake", "llvm")
 
 # 2. Get a scratch directory
 scratch_dir = get_scratch!(LLVMExtra_jll, "build")
+isdir(scratch_dir) && rm(scratch_dir; recursive=true)
 source_dir = joinpath(@__DIR__, "LLVMExtra")
 
+# Build!
 @info "Building" source_dir scratch_dir LLVM_DIR
-
 run(`cmake -DLLVM_DIR=$(LLVM_DIR) -B$(scratch_dir) -S$(source_dir)`)
 run(`cmake --build $(scratch_dir)`)
 
-lib_path = joinpath(scratch_dir, "lib", basename(LLVMExtra_jll.libLLVMExtra_path))
-
-if !isfile(lib_path)
-    error("Could not find library $lib_path in build directory")
+# Discover built libraries
+built_libs = filter(readdir(joinpath(scratch_dir, "lib"))) do file
+    endswith(file, ".$(Libdl.dlext)")
 end
+lib_path = joinpath(scratch_dir, "lib", only(built_libs))
+isfile(lib_path) || error("Could not find library $lib_path in build directory")
 
 # Tell LLVMExtra_jll to load our library instead of the default artifact one
 set_preferences!(
