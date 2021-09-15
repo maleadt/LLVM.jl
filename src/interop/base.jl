e8e17ddc6de63609de9d3973cdad978a8d48a206
Tim Besard <tim.besard@gmail.com>
2021-09-15 10:18:18 +0200

    Use at-inline instead of _inline_meta.

--- src/interop/base.jl
+++ src/interop/base.jl
@@ -34,9 +34,16 @@ function call_function(llvmf::LLVM.Function, rettyp::Type=Nothing, argtyp::Type=
     ir = string(mod)
     fn = LLVM.name(llvmf)
     @assert !isempty(fn)
-    quote
-        Base.@_inline_meta
-        Base.llvmcall(($ir,$fn), $rettyp, $argtyp, $(args...))
+    if VERSION >= v"1.8.0-DEV.410"
+        quote
+            Base.@inline
+            Base.llvmcall(($ir,$fn), $rettyp, $argtyp, $(args...))
+        end
+    else
+        quote
+            Base.@_inline_meta
+            Base.llvmcall(($ir,$fn), $rettyp, $argtyp, $(args...))
+        end
     end
 end
 
