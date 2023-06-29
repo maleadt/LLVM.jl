b349b0807fe5530c9ce450d73dabdd0c454bad14
Prem Chintalapudi <prem.chintalapudi@gmail.com>
2023-06-29 16:11:21 -0400

    Custom pass functions

---
+++ src/newpm/utilities.jl
@@ -0,0 +1,26 @@
+@checked struct PreservedAnalyses
+    ref::API.LLVMPreservedAnalysesRef
+end
+
+Base.unsafe_convert(::Type{API.LLVMPreservedAnalysesRef}, pa::PreservedAnalyses) = pa.ref
+
+no_analyses_preserved() = PreservedAnalyses(API.LLVMCreatePreservedAnalysesNone())
+
+all_analyses_preserved() = PreservedAnalyses(API.LLVMCreatePreservedAnalysesAll())
+
+cfg_analyses_preserved() = PreservedAnalyses(API.LLVMCreatePreservedAnalysesCFG())
+
+function unsafe_run(f::Core.Function)
+    try
+        return f()
+    catch e
+        # Throwing exceptions in the pass pipeline is very unsafe
+        # and will result in undefined behavior immediately. This
+        # try-catch just tries to report the error so that the
+        # issue is at least debuggable.
+        io = IOBuffer()
+        Base.showerror(io, e)
+        error_msg = String(take!(io))
+        @error error_msg
+    end
+end
