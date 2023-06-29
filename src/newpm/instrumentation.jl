a1bdff275b4415e96753b6f5c76f9c423ea8eddf
Prem Chintalapudi <prem.chintalapudi@gmail.com>
2023-06-29 12:37:05 -0400

    Instrumentation functions

--- src/newpm/instrumentation.jl
+++ src/newpm/instrumentation.jl
@@ -1,3 +1,30 @@
-struct PassInstrumentationCallbacks
+@checked struct PassInstrumentationCallbacks
+    ref::API.LLVMPassInstrumentationCallbacksRef
+    si::API.LLVMStandardInstrumentationsRef
     roots::Vector{Any}
+end
+
+Base.unsafe_convert(::Type{API.LLVMPassInstrumentationCallbacksRef}, pic::PassInstrumentationCallbacks) = pic.ref
+
+PassInstrumentationCallbacks() = PassInstrumentationCallbacks(API.LLVMCreatePassInstrumentationCallbacks(), C_NULL, [])
+
+function PassInstrumentationCallbacks(f::Core.Function, args...; kwargs...)
+    pic = PassInstrumentationCallbacks(args...; kwargs...)
+    try
+        f(pic)
+    finally
+        dispose(pic)
+    end
+end
+
+function dispose(pic::PassInstrumentationCallbacks)
+    API.LLVMDisposePassInstrumentationCallbacks(pic.ref)
+    if pic.si != C_NULL
+        API.LLVMDisposeStandardInstrumentations(pic.si)
+    end
+end
+
+function standard_instrumentations!(pic::PassInstrumentationCallbacks)
+    pic.si = API.LLVMCreateStandardInstrumentations()
+    API.LLVMAddStandardInstrumentations(pic.ref, pic.si)
 end
