93d44c564e6c32f76946016c657024ae30ede06c
Tim Besard <tim.besard@gmail.com>
2023-09-29 12:35:11 +0200

    Don't include Legacy PM pass functionality when not useful.

--- src/interop.jl
+++ src/interop.jl
@@ -5,10 +5,13 @@ import ..LLVM: API
 
 include("interop/base.jl")
 include("interop/asmcall.jl")
-include("interop/passes.jl")
 include("interop/pointer.jl")
 include("interop/utils.jl")
 include("interop/intrinsics.jl")
+
+if VERSION < v"1.11.0-DEV.428"
+    include("interop/passes.jl")
+end
 
 if LLVM.has_newpm() && VERSION >= v"1.10.0-DEV.1622"
     include("interop/newpm.jl")
