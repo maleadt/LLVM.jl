9ba797aaefebb8983f3f3bacc80feaf0b4764d7f
Tim Besard <tim.besard@gmail.com>
2024-06-06 18:58:02 +0200

    Add deprecation for Enzyme.jl.

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -6,3 +6,5 @@ Base.@deprecate called_value(inst::CallBase) called_operand(inst)
 
 has_orc_v1() = false
 has_orc_v2() = true
+
+Base.@deprecate_binding InstructionMetadataDict LLVM.ValueMetadataDict
