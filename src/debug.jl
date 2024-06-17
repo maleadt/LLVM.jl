20206c2a81b726a04651764334762f12428c4856
Tim Besard <tim.besard@gmail.com>
2024-06-17 10:46:39 +0200

    Improve reporting.

--- src/debug.jl
+++ src/debug.jl
@@ -11,30 +11,69 @@ const tracked_objects = Dict{Any,Any}()
 
 function mark_alloc(obj::Any)
     @static if memcheck_enabled
-        tracked_objects[obj] = backtrace()[2:end]
+        io = Core.stdout
+        new_alloc_bt = backtrace()[2:end]
+
+        if haskey(tracked_objects, obj)
+            old_alloc_bt, dispose_bt = tracked_objects[obj]
+            if dispose_bt == nothing
+                print("\nWARNING: An instance of $(typeof(obj)) was not properly disposed of, and a new allocation will overwrite it.")
+                print("\nThe original allocation was at:")
+                Base.show_backtrace(io, old_alloc_bt)
+                print("\nThe new allocation is at:")
+                Base.show_backtrace(io, new_alloc_bt)
+                println(io)
+            end
+        end
+
+        tracked_objects[obj] = (new_alloc_bt, nothing)
     end
     return obj
 end
 
 function mark_dispose(obj)
     @static if memcheck_enabled
-        delete!(tracked_objects, obj)
+        io = Core.stdout
+        new_dispose_bt = backtrace()[2:end]
+
+        if !haskey(tracked_objects, obj)
+            print(io, "\nWARNING: An unknown instance of $(typeof(obj)) is being disposed of.")
+            Base.show_backtrace(io, new_dispose_bt)
+            return obj
+        end
+
+        alloc_bt, old_dispose_bt = tracked_objects[obj]
+        if old_dispose_bt !== nothing
+            print("\nWARNING: An instance of $(typeof(obj)) is being disposed twice.")
+            print("\nThe object was allocated at:")
+            Base.show_backtrace(io, alloc_bt)
+            print("\nThe object was already disposed at:")
+            Base.show_backtrace(io, old_dispose_bt)
+            print("\nThe object is being disposed again at:")
+            Base.show_backtrace(io, new_dispose_bt)
+            println(io)
+        end
+
+        tracked_objects[obj] = (alloc_bt, new_dispose_bt)
     end
     return obj
 end
 
 function report_leaks(code)
-    # if we errorred, we can't trust the memory state
+    # if we errored, we can't trust the memory state
     if code != 0
         return
     end
 
     @static if memcheck_enabled
         io = Core.stdout
-        for (obj, bt) in tracked_objects
-            print(io, "WARNING: An instance of $(typeof(obj)) was not properly disposed of.")
-            Base.show_backtrace(io, bt)
-            println(io)
+        for (obj, (alloc_bt, dispose_bt)) in tracked_objects
+            if dispose_bt === nothing
+                print(io, "\nWARNING: An instance of $(typeof(obj)) was not properly disposed of.")
+                print("\nThe object was allocated at:")
+                Base.show_backtrace(io, alloc_bt)
+                println(io)
+            end
         end
     end
 end
