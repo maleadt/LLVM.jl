b78e3b6bb7f3ef55e9e3bf44973e7c77c0df232e
Tim Besard <tim.besard@gmail.com>
2024-08-29 11:17:02 +0200

    Fix and test memcheck.

--- test/support_tests.jl
+++ test/support_tests.jl
@@ -1,5 +1,22 @@
 @testitem "support" begin
 
+function execute_code(code)
+    script = """
+        using LLVM
+        $code"""
+    cmd = `$(Base.julia_cmd()) --project=$(Base.active_project()) -e $script`
+
+    out = Pipe()
+    err = Pipe()
+    proc = run(pipeline(cmd, stdout=out, stderr=err), wait=false)
+    close(out.in)
+    close(err.in)
+    wait(proc)
+    out = read(out, String)
+    err = read(err, String)
+    return (; out, err)
+end
+
 @testset "command-line options" begin
 
 code = """
@@ -7,13 +24,42 @@ code = """
     LLVM.clopts("-version")
 """
 
-out = Pipe()
-cmd = `$(Base.julia_cmd()) --project=$(Base.active_project())`
-run(pipeline(`$cmd -e $code`, stdout=out, stderr=out))
-close(out.in)
+(; out, err) = execute_code("LLVM.clopts(\"-version\")")
+@test occursin("LLVM (http://llvm.org/)", out)
 
-@test occursin("LLVM (http://llvm.org/)", read(out, String))
+end
 
+@testset "memcheck" begin
+if LLVM.memcheck_enabled
+    # use after dispose
+    let (; out, err) =
+        execute_code("""buf = LLVM.MemoryBuffer(UInt8[])
+                        dispose(buf)
+                        length(buf)""")
+        @test occursin("An instance of MemoryBuffer is being used after it was disposed of.", out)
+    end
+
+    # double dispose
+    let (; out, err) =
+        execute_code("""buf = LLVM.MemoryBuffer(UInt8[])
+                        dispose(buf)
+                        dispose(buf)""")
+        @test occursin("An instance of MemoryBuffer is being disposed of twice.", out)
+    end
+
+    # unrelated dispose
+    let (; out, err) =
+        execute_code("""buf = LLVM.MemoryBuffer(LLVM.API.LLVMMemoryBufferRef(1))
+                        dispose(buf)""")
+        @test occursin("An unknown instance of MemoryBuffer is being disposed of.", out)
+    end
+
+    # missing dispose
+    let (; out, err) =
+        execute_code("""buf = LLVM.MemoryBuffer(UInt8[])""")
+        @test occursin("An instance of MemoryBuffer was not properly disposed of.", out)
+    end
+end
 end
 
 end
