69057aeab348e490697e5a7acdb5caf6176ffe45
Tim Besard <tim.besard@gmail.com>
2024-09-04 18:55:28 +0200

    Simplify and test instruction predicate getter.

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -34,6 +34,9 @@ Base.@deprecate_binding ValueMetadataDict LLVM.InstructionMetadataDict
 @deprecate unsafe_delete!(::Function, bb::BasicBlock) erase!(bb)
 @deprecate unsafe_delete!(::BasicBlock, inst::Instruction) erase!(inst)
 
+@deprecate predicate_int(inst) predicate(inst)
+@deprecate predicate_real(inst) predicate(inst)
+
 @deprecate Base.string(md::MDString) convert(String, md) false
 function Base.show(io::IO, ::MIME"text/plain", md::MDString)
     str = @invoke string(md::Metadata)
